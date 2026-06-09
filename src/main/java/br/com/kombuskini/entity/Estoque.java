69d271850ffa175a3c11143b48d35ceee4526087
Bruno Lira <bclira21@gmail.com>
2026-06-09 20:37:32 -0300

    Início da classe ItemPedido

--- src/main/java/br/com/kombuskini/entity/Estoque.java
+++ src/main/java/br/com/kombuskini/entity/Estoque.java
@@ -1,11 +1,13 @@
 package br.com.kombuskini.entity;
 
+import java.util.List;
+
 public class Estoque {
     private int id;
-    private Kombuskini kombuskini;
+    private List<Kombuskini> kombuskini;
     private int quantidadeAtual;
 
-    public Estoque(int id, Kombuskini kombuskini, int quantidadeAtual) {
+    public Estoque(int id, List<Kombuskini> kombuskini, int quantidadeAtual) {
         this.id = id;
         this.kombuskini = kombuskini;
         this.quantidadeAtual = quantidadeAtual;
--- src/main/java/br/com/kombuskini/entity/Pedido.java
+++ src/main/java/br/com/kombuskini/entity/Pedido.java
@@ -1,4 +1,28 @@
 package br.com.kombuskini.entity;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Pedido {
+    public enum StatusPedido{
+        RASCUNHO,
+        REGISTRADO,
+        ENVIADO,
+        FINALIZADO
+    }
+
+    private int id;
+    private Cliente cliente;
+    private final List<ItemPedido> itens = new ArrayList<>();
+    private StatusPedido status = StatusPedido.RASCUNHO;
+
+    public Pedido(int id, Cliente cliente){
+        if(cliente == null) throw  new IllegalArgumentException("O cliente é obrigatório no novo pedido!");
+        this.id = id;
+        this.cliente = cliente;
+    }
+
+    public void adicionarProduto(Kombuskini produto, int quantidade){
+
+    }
 }
