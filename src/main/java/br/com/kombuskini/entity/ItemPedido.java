ef9c1a34a2e009e6b1a96ddf19e16dd8f19ec5a5
andrew-pravoslavie <gabriel.andrewbg@gmail.com>
2026-06-09 20:23:12 -0300

    Parte das entidades (regras de negócio) feitas

--- src/main/java/br/com/kombuskini/entity/ItemPedido.java
+++ src/main/java/br/com/kombuskini/entity/ItemPedido.java
@@ -1,5 +1,36 @@
 package br.com.kombuskini.entity;
 
 public class ItemPedido {
+    private Kombuskini kombuskini;
+    private int quantidade;
 
+    public ItemPedido(Kombuskini kombuskini, int quantidade) {
+        this.kombuskini = kombuskini;
+        this.quantidade = quantidade;
+    }
+
+    public void adicionar(int n){
+        if(n <= 0){
+            throw new IllegalArgumentException("A quantidade a ser adicionada deve ser maior que zero");
+        }
+        this.quantidade = quantidade + n;
+    }
+
+    public void remover(int n){
+        if(n <= 0){
+            throw new IllegalArgumentException("A quantidade a ser removida deve ser maior que zero");
+        }
+        if(quantidade < n){
+            throw new IllegalArgumentException("Estoque insuficiente");
+        }
+        this.quantidade = quantidade - n;
+    }
+
+    public Kombuskini getKombuskini() {
+        return kombuskini;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
 }
