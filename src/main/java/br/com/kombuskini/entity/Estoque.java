ec7e5be07edc59087db9ee3a0d4f75b2152b0c06
Bruno Lira <bclira21@gmail.com>
2026-06-09 22:04:37 -0300

    Finalização das classes Estoque e ItemEstoque.

--- src/main/java/br/com/kombuskini/entity/Estoque.java
+++ src/main/java/br/com/kombuskini/entity/Estoque.java
@@ -8,8 +8,49 @@ public class Estoque {
     private int quantidadeAtual;
 
     public Estoque(int id, List<Kombuskini> kombuskini, int quantidadeAtual) {
+        if(quantidadeAtual > 0){
+            this.id = id;
+            this.kombuskini = kombuskini;
+            this.quantidadeAtual = quantidadeAtual;
+        } else{
+            throw new IllegalArgumentException ("Deve ser informada alguma quantidade no novo estoque!");
+        }
+    }
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
         this.id = id;
+    }
+
+    public List<Kombuskini> getKombuskini() {
+        return kombuskini;
+    }
+
+    public void setKombuskini(List<Kombuskini> kombuskini) {
         this.kombuskini = kombuskini;
+    }
+
+    public int getQuantidadeAtual() {
+        return quantidadeAtual;
+    }
+
+    public void setQuantidadeAtual(int quantidadeAtual) {
         this.quantidadeAtual = quantidadeAtual;
+    }
+
+    public void registrarEntrada(int qtd){
+        quantidadeAtual += qtd;
+    }
+
+    public void registrarSaida(int qtd){
+        if(qtd > quantidadeAtual){
+            throw new IllegalStateException("Não há peças o suficiente no estoque");
+        } else {
+            quantidadeAtual -= qtd;
+        }
     }
 }
--- src/main/java/br/com/kombuskini/entity/ItemEstoque.java
+++ src/main/java/br/com/kombuskini/entity/ItemEstoque.java
@@ -1,12 +1,25 @@
 package br.com.kombuskini.entity;
 
+import java.time.LocalDate;
+import java.util.Locale;
+
 public class ItemEstoque {
     private Kombuskini kombuskini;
     private int quantidade;
+    private LocalDate dataVenda;
 
-    public ItemEstoque(Kombuskini kombuskini, int quantidade) {
+    public ItemEstoque(Kombuskini kombuskini, int quantidade, LocalDate dataVenda) {
+        if(kombuskini == null){
+            throw  new IllegalArgumentException("Deve-se informar algum kombuskini.");
+        }
+
+        if(quantidade <= 0){
+            throw new IllegalArgumentException("Deve-se informar alguma quantidade positiva");
+        }
+
         this.kombuskini = kombuskini;
         this.quantidade = quantidade;
+        this.dataVenda = dataVenda;
     }
 
     public Kombuskini getKombuskini() {
